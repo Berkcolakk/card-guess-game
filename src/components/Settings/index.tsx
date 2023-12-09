6630357b1b3b15d57eb39603766d010c7e0af033
Berk Çolak <berkcolak4@gmail.com>
2023-12-09 18:32:55 +0300

    - language setting added

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import CustomModal from "../CustomModal"
-import { Segmented, Space } from "antd";
+import { Segmented, Select, Space, message } from "antd";
 import Title from "antd/es/typography/Title";
 import { BugOutlined, RobotOutlined, SmileOutlined } from "@ant-design/icons";
 import { SegmentedValue } from "antd/es/segmented";
@@ -12,26 +12,43 @@ interface IProps {
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 const Settings: React.FC<IProps> = observer(({ open, setOpen }) => {
+    const successMsg = () => message.success("Ayarlarınız başarıyla güncellenmiştir.")
     return (
         <CustomModal
-            title={ <Title level={3} className="!mb-0">Ayarlar</Title>}
+            title={<Title level={3} className="!mb-0">Ayarlar</Title>}
             open={open}
             setOpen={setOpen}>
             <>
-                <div className="flex">
-                    <Space>
-                        <Title level={5} className="!mb-0">Oyun modu: </Title>
-                        <Segmented value={gameOptionModule.mode} options={[
-                            { label: "Kolay", value: "easy", icon: <SmileOutlined /> },
-                            { label: "Normal", value: "normal", icon: <RobotOutlined /> },
-                            { label: "Zor", value: "hard", icon: <BugOutlined /> },
+                <Space className="grid grid-cols-3">
+                    <Title level={5} className="!mb-0">Oyun modu: </Title>
+                    <Segmented value={gameOptionModule.mode} options={[
+                        { label: "Kolay", value: "easy", icon: <SmileOutlined /> },
+                        { label: "Normal", value: "normal", icon: <RobotOutlined /> },
+                        { label: "Zor", value: "hard", icon: <BugOutlined /> },
+                    ]}
+                        onChange={(value: SegmentedValue) => {
+                            gameOptionModule.setMode(value as TModeTypes)
+                            localStorage.setItem("mode", value as TModeTypes)
+                            successMsg();
+                        }} />
+                </Space>
+                <Space className="grid grid-cols-3">
+                    <Title level={5} className="!mb-0">Dil: </Title>
+                    <Select
+                        defaultValue={gameOptionModule.language}
+                        onChange={(value: string) => {
+                            gameOptionModule.setLanguage(value)
+                            localStorage.setItem("language", value)
+                            successMsg();
+                        }}
+                        size="middle"
+                        style={{ width: '100%' }}
+                        options={[
+                            { value: 'tr', label: 'Türkçe' },
+                            { value: 'en', label: 'İngilizce' },
                         ]}
-                            onChange={(value: SegmentedValue) => {
-                                gameOptionModule.setMode(value as TModeTypes)
-                                localStorage.setItem("mode", value as TModeTypes)
-                            }} />
-                    </Space>
-                </div>
+                    />
+                </Space>
             </>
         </CustomModal>
     )
--- src/store/GameOptionStore.ts
+++ src/store/GameOptionStore.ts
@@ -4,10 +4,13 @@ export type TModeTypes = "easy" | "normal" | "hard";
 
 const gameOptionModule = {
   mode: localStorage?.mode,
-
+  language: localStorage?.language ?? "tr",
   setMode(payload: TModeTypes) {
     this.mode = payload;
-  }
+  },
+  setLanguage(payload: string) {
+    this.language = payload;
+  },
 };
 
 makeAutoObservable(gameOptionModule);
