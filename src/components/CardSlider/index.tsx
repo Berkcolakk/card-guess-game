7a4e2b20bb15d648f9c1b1d4301a62d0ceafa8ca
Berk Çolak <berkcolak4@gmail.com>
2023-12-18 03:37:59 +0300

    - added tooltip

--- src/components/CardSlider/index.tsx
+++ src/components/CardSlider/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Cards from "@/data/yugiohCards.json";
 import Image from "next/image";
-import { Card } from 'antd';
+import { Card, Tooltip } from 'antd';
 import Meta from "antd/es/card/Meta";
 import Marquee from "react-fast-marquee";
 
@@ -10,13 +10,16 @@ const CardPresentation = () => {
         <Marquee >
             {Cards.map((item, index) => {
                 return (
-                    <Card
-                        key={index}
-                        className="!w-44 m-2"
-                        cover={<Image src={"/" + item.imagePath} alt={item.name} priority={true} height={208} quality={100} width={160} className="h-52 w-40" />}
-                    >
-                        <Meta title={item.name} />
-                    </Card>
+                    <Tooltip key={index} placement="topLeft" title={item.name}>
+                        <Card
+                            
+                            className="!w-44 m-2"
+                            cover={
+                                <Image src={"/" + item.imagePath} alt={item.name} priority={true} height={208} quality={100} width={160} className="h-52 w-40" />
+                            }>
+                            <Meta title={item.name} />
+                        </Card>
+                    </Tooltip>
                 )
             })}
         </Marquee>
